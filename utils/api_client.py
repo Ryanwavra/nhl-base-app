dbf26a9d8437d6ffddce99b23c671e8ac98b4e68
ryanwavra <ryanew06@gmail.com>
2025-10-14 22:19:11 -0400

    Initial commit: projeect scaffold and .gitignore

---
+++ utils/api_client.py
@@ -0,0 +1,14 @@
+class ApiClient:
+
+    #method 1 getting upcoming games
+    def get_upcoming_games(self):
+        pass
+
+
+
+
+
+
+    #method 2 getting completed games
+    def get_completed_games(self):
+        pass
---
+++ utils/mock_api_client.py
@@ -0,0 +1,11 @@
+class ApiClient:
+
+    def get_upcoming_games(self, date=None):
+        #later: load and return upcoming games.json
+
+        pass
+
+    def get_completed_games(self, date=None):
+        #later load and return completed games json
+
+        pass
